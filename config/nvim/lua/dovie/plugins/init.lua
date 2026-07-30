63f5aa6415fae4001dd4aa7fea0d75575185fd7f
Dovie Weinstock <DovieWeinstock@gmail.com>
2026-07-30 06:22:32 -0400

    remove obsolete auto-save message option

--- config/nvim/lua/dovie/plugins/init.lua
+++ config/nvim/lua/dovie/plugins/init.lua
@@ -253,7 +253,6 @@ return {
     event = { "InsertLeave", "TextChanged" },
     opts = {
       enabled = true,
-      execution_message = { enabled = false },
       trigger_events = {
         immediate_save = { "BufLeave", "FocusLost" },
         defer_save = { "InsertLeave", "TextChanged" },
